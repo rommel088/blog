7a067be52415fda1d1f44b8609878dde6c59d374
rommel <rommel0888@gmail.com>
2014-01-06 01:37:54 +0200

    all posts, post, sidebar, menu

--- src/Blog/BlogBundle/Controller/MainController.php
+++ src/Blog/BlogBundle/Controller/MainController.php
@@ -14,79 +14,95 @@ class MainController extends Controller
 {
     public function indexAction()
     {
-        echo "112312412414124";
-        exit;
-        $viewPlanes = $this->container->get('ViewPlanes');
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT p
+            FROM BlogBundle:Articles p
+            ORDER BY p.created ASC'
+        )->setMaxResults($this->container->getParameter('articles_per_page'));
 
-        $planes = $viewPlanes->getAllPlanes();
-        $engines = $viewPlanes->getAllEngines();
-        $weapons = $viewPlanes->getAllWeapons();
+        $articles = $query->getResult();
+        $result = "";
+        foreach($articles as $key=>$value){
+            $result[$key]['id'] = $value->getId();
+            $result[$key]['title'] = $value->getTitle();
+            $result[$key]['image'] = $value->getImage();
+            $result[$key]['body'] = $value->getBody();
+            $result[$key]['viewed'] = $value->getViewed();
+            $result[$key]['created'] = $value->getCreated()->format('Y-m-d H:i:s');
+            $result[$key]['updated'] = $value->getCreated()->format('Y-m-d H:i:s');
+        }
 
-        $merged = array_merge($engines, $weapons);
-        $keys = array_rand($merged, 3);
-        $features[] = $merged[$keys[0]];
-        $features[] = $merged[$keys[1]];
-        $features[] = $merged[$keys[2]];
-
-        return $this->render('AcmeWork6Bundle:Planes:home.html.twig', array('planes' => $planes,
-                                                                            'features' => $features));
+        return $this->render('BlogBundle::home.html.twig', array('articles' => $result,
+                                                                 'sidebar' => $this->sidebarDataAction()->getContent()));
     }
 
-    public function planesAction()
+    public function articleAction($id)
     {
-        $viewPlanes = $this->container->get('ViewPlanes');
-        $planes = $viewPlanes->getAllPlanes();
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            "SELECT p
+            FROM BlogBundle:Articles p
+            WHERE p.id = ".$id."
+            ORDER BY p.created ASC"
+        )->setMaxResults(1);
+        $articles = $query->getResult();
 
-        return $this->render('AcmeWork6Bundle:Planes:category.html.twig', array('content' => $planes,
-                                                                                'source' => 'plane'));
+        $result = "";
+        foreach($articles as $key=>$value){
+            $result[$key]['id'] = $value->getId();
+            $result[$key]['title'] = $value->getTitle();
+            $result[$key]['image'] = $value->getImage();
+            $result[$key]['body'] = $value->getBody();
+            $result[$key]['viewed'] = $value->getViewed();
+            $result[$key]['created'] = $value->getCreated()->format('Y-m-d H:i:s');
+            $result[$key]['updated'] = $value->getCreated()->format('Y-m-d H:i:s');
+        }
+        return $this->render('BlogBundle::article.html.twig', array('article' => $result,
+                                                                    'sidebar' => $this->sidebarDataAction()->getContent()));
     }
 
-    public function enginesAction()
+    public function sidebarDataAction()
     {
-        $viewPlanes = $this->container->get('ViewPlanes');
-        $engines = $viewPlanes->getAllEngines();
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT p
+            FROM BlogBundle:Articles p
+            ORDER BY p.created ASC'
+        )->setMaxResults($this->container->getParameter('items_in_sidebar'));
+        $articles = $query->getResult();
+        $byCreation = "";
+        foreach($articles as $key=>$value){
+            $byCreation[$key]['id'] = $value->getId();
+            $byCreation[$key]['title'] = $value->getTitle();
+        }
 
-        return $this->render('AcmeWork6Bundle:Planes:category.html.twig', array('content' => $engines,
-            'source' => 'engine'));
-    }
+        $query = $em->createQuery(
+            'SELECT p
+            FROM BlogBundle:Articles p
+            ORDER BY p.viewed ASC'
+        )->setMaxResults($this->container->getParameter('items_in_sidebar'));
+        $articles = $query->getResult();
+        $byViewed = "";
+        foreach($articles as $key=>$value){
+            $byViewed[$key]['id'] = $value->getId();
+            $byViewed[$key]['title'] = $value->getTitle();
+        }
 
-    public function weaponsAction()
-    {
-        $viewPlanes = $this->container->get('ViewPlanes');
-        $weapons = $viewPlanes->getAllWeapons();
-
-        return $this->render('AcmeWork6Bundle:Planes:category.html.twig', array('content' => $weapons,
-            'source' => 'weapon'));
-    }
-
-    public function planeAction($id)
-    {
-        $viewPlanes = $this->container->get('ViewPlanes');
-        $planes = $viewPlanes->getPlane($id);
-        return $this->render('AcmeWork6Bundle:Planes:craft.html.twig', array('plane' => $planes));
-    }
-
-    public function engineAction($id)
-    {
-        $viewPlanes = $this->container->get('ViewPlanes');
-        $engines = $viewPlanes->getEngine($id);
-
-        return $this->render('AcmeWork6Bundle:Planes:engine.html.twig', array('engine' => $engines, 'planes' => $engines['planes']));
-    }
-
-    public function weaponAction($id)
-    {
-        $viewPlanes = $this->container->get('ViewPlanes');
-        $weapons = $viewPlanes->getWeapon($id);
-        return $this->render('AcmeWork6Bundle:Planes:weapons.html.twig', array('weapons' => $weapons));
-    }
-
-    public function contactAction()
-    {
-        $dispatcher = new EventDispatcher();
-        $listener = new Request();
-        $dispatcher->addListener('kernel.controller', array($listener, 'onFooAction'));
-        return $this->render('AcmeWork6Bundle:Planes:contact.html.twig');
+        $query = $em->createQuery(
+            'SELECT p
+            FROM BlogBundle:GuestPosts p
+            ORDER BY p.created ASC'
+        )->setMaxResults($this->container->getParameter('items_in_sidebar'));
+        $articles = $query->getResult();
+        $posts = "";
+        foreach($articles as $key=>$value){
+            $posts[$key]['id'] = $value->getId();
+            $posts[$key]['title'] = $value->getMessage();
+        }
+        return $this->render('BlogBundle::sidebar.html.twig', array('byCreation' => $byCreation,
+                                                                    'byViewed' => $byViewed,
+                                                                    'posts' => $posts));
     }
 
     public function mailAction()
@@ -109,13 +125,6 @@ class MainController extends Controller
 
     public function guestAction(Request $request)
     {
-        $viewPlanes = $this->container->get('GuestBook');
-
-        if ($request->get("action") == 'delMessage') {
-            $viewPlanes->delMessages($request->get("idMessage"));
-            return new Response(1);
-        }
-        $messages = $viewPlanes->getAllMessages();
 
         $guest = new Forms\Guest();
         $form = $this->createFormBuilder($guest)
@@ -128,9 +137,26 @@ class MainController extends Controller
         $form->handleRequest($request);
         if ($form->isValid()) {
 
-            $viewPlanes->addMessage($form->getData());
+//            $viewPlanes->addMessage($form->getData());
             return $this->redirect($this->generateUrl('guest'));
 
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT p
+            FROM BlogBundle:GuestPosts p
+            ORDER BY p.created ASC'
+        )->setMaxResults($this->container->getParameter('posts_per_page'));
+
+        $message = $query->getResult();
+
+        foreach ($message as $key => $val) {
+            $messages[$key]['id'] = $val->getId();
+            $messages[$key]['name'] = $val->getName();
+            $messages[$key]['email'] = $val->getEmail();
+            $messages[$key]['message'] = $val->getMessage();
+            $messages[$key]['created'] = $val->getCreated()->format('Y-m-d H:i:s');
         }
 
         $page = $request->get('page');
@@ -147,13 +173,13 @@ class MainController extends Controller
             throw new NotFoundHttpException();
         }
 
-        $pagerfanta->setMaxPerPage($this->container->getParameter('element_per_page'));
+        $pagerfanta->setMaxPerPage($this->container->getParameter('posts_per_page'));
         $pagerfanta->setCurrentPage($page);
         $messages = $pagerfanta->getCurrentPageResults();
 
 
 
-        return $this->render('AcmeWork6Bundle:Planes:guest.html.twig', array(
+        return $this->render('BlogBundle::guest.html.twig', array(
             'form' => $form->createView(),
             'messages' => $messages,
             'pagerfanta' => $pagerfanta,
---
+++ src/Blog/BlogBundle/Forms/Guest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Acme\Work6Bundle\Forms;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Guest
+{
+    /**
+     * @Assert\Regex(
+     *     pattern="/^[a-zA-ZА-Яа-я]+$/",
+     *     message="Your name must contain only letters"
+     * )
+     */
+    protected $name;
+
+    /**
+     * @Assert\Email(
+     *     message = "The email '{{ value }}' is not a valid email.",
+     *     checkMX = true
+     * )
+     */
+    protected $email;
+
+    /**
+     * @Assert\Length(
+     *      min = "100"
+     * )
+     */
+    protected $message;
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+} 
