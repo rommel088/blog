9aaaafbd2fd3e293d68324f4a4686bec5c0896f3
rommel <rommel0888@gmail.com>
2014-02-07 09:36:55 +0200

    login page

--- src/Blog/BlogBundle/Controller/BackendController.php
+++ src/Blog/BlogBundle/Controller/BackendController.php
@@ -6,10 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Blog\BlogBundle\Forms;
-use Pagerfanta\Pagerfanta;
 use Symfony\Component\EventDispatcher\EventDispatcher;
-use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Blog\BlogBundle\Entity;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class BackendController extends Controller
 {
@@ -54,5 +53,43 @@ class BackendController extends Controller
         $em->flush();
 
         return new Response(1);
+    }
+
+    public function loginAction(Request $request)
+    {
+        /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
+        $session = $request->getSession();
+
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        if ($error) {
+            // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
+            $error = $error->getMessage();
+        }
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
+
+        $csrfToken = $this->container->has('form.csrf_provider')
+            ? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate')
+            : null;
+
+        return $this->render('BlogBundle:Backend:login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+            'csrf_token' => $csrfToken,
+        ));
+//        return $this->renderLogin(array(
+//            'last_username' => $lastUsername,
+//            'error'         => $error,
+//            'csrf_token' => $csrfToken,
+//        ));
     }
 } 
